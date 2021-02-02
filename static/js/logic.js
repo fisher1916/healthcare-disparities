07c7c5817117d373f29ea42e028b6d30e1643b6d
Jay Hastings <jayhjman@gmail.com>
2021-02-02 15:22:00 -0800

    somehow last commit got lost, readding

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,8 +1,19 @@
 // local url for mortality rates
 var url = "/mortality";
 
+function getSortedElementsByKey(targetData, key) {
+  var returnArray = [
+    ...new Map(targetData.map((item) => [item[key], item])).values(),
+  ];
+  return returnArray.sort((a, b) => (a[key] > b[key] ? 1 : -1));
+}
 // load the data into the data variable
 d3.json(url).then((data) => {
   console.log("records read:" + data.length);
   console.log(data);
+
+  const uniqueStates = getSortedElementsByKey(data, "state_name");
+  const uniqueMeasures = getSortedElementsByKey(data, "measure");
+  console.log(uniqueStates);
+  console.log(uniqueMeasures);
 });
