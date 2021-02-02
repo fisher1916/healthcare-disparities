6fe0dd62ef78ba56e7b2c3969eb3025fb5658e0d
Jay Hastings <jayhjman@gmail.com>
2021-02-02 00:22:47 -0800

    added logic to read from the sqlite database via app.py and return payload

--- static/js/logic.js
+++ static/js/logic.js
@@ -1,1 +1,8 @@
-//put your code here!!!
+// local url for mortality rates
+var url = "/mortality";
+
+// load the data into the data variable
+d3.json(url).then((data) => {
+  console.log("records read:" + data.length);
+  console.log(data);
+});
