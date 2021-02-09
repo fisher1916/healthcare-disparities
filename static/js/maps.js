a55d549ba35177b702d1458e8e57015e6e929d9a
Josh <JMWilliams3790@gmail.com>
2021-02-08 19:25:51 -0800

    got skeleton of legend working

--- static/js/maps.js
+++ static/js/maps.js
@@ -136,4 +136,26 @@ d3.json("/getallmortalities").then((data) => {
       collapsed: false,
     })
     .addTo(myMap);
+
+    var legend = L.control({ position: "bottomright" });
+
+    legend.onAdd = function (map) {
+      var div = L.DomUtil.create("div", "info legend"),
+        grades = [-10, 10, 30, 50, 70, 90],
+        labels = [];
+      colors = ["green", "yellowgreen", "yellow", "orange", "orangered", "red"];
+      // loop through our density intervals and generate a label with a colored square for each interval
+      for (var i = 0; i < grades.length; i++) {
+        div.innerHTML +=
+          '<i style="background:' +
+          colors[i] +
+          '"></i> ' +
+          grades[i] +
+          (grades[i + 1] ? "&ndash;" + grades[i + 1] + "<br>" : "+");
+      }
+  
+      return div;
+    };
+    legend.addTo(myMap);
+
 });
